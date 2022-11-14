421f833485694e61afe676aafcf684f8166838c1
e.kamelyanov <eldar.kamelyanov@advalange.com>
2022-11-14 16:44:31 +0300

    upd

---
+++ Store.Services.ProductAPI/Controllers/ProductApiController.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Mvc;
+using Store.Services.ProductAPI.Models.Dto;
+using Store.Services.ProductAPI.Repository;
+
+namespace Store.Services.ProductAPI.Controllers;
+
+[Route("api/products")]
+public class ProductApiController : ControllerBase
+{
+    protected ResponseDto _response;
+    private IProductRepository _productRepository;
+
+    public ProductApiController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+        this._response = new ResponseDto();
+    }
+
+    [HttpGet]
+    public async Task<object> Get()
+    {
+        try
+        {
+            IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+            _response.Result = productDtos;
+        }
+        catch (Exception e)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string>() { e.ToString() };
+        }
+
+        return _response;
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<object> Get(int id)
+    {
+        try
+        {
+            ProductDto productDto = await _productRepository.GetProductById(id);
+            _response.Result = productDto;
+        }
+        catch (Exception e)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string>() { e.Message.ToString() };
+        }
+
+        return _response;
+    }
+
+    [HttpPost]
+    public async Task<object> Post([FromBody] ProductDto productDtoData)
+    {
+        try
+        {
+            ProductDto productDto = await _productRepository.CreateUpdateProduct(productDtoData);
+            _response.Result = productDto;
+        }
+        catch(Exception e)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string>() { e.Message.ToString() };
+        }
+
+        return _response;
+    }
+    
+    [HttpPut]
+    public async Task<object> Put([FromBody] ProductDto productDtoData)
+    {
+        try
+        {
+            ProductDto productDto = await _productRepository.CreateUpdateProduct(productDtoData);
+            _response.Result = productDto;
+        }
+        catch(Exception e)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string>() { e.Message.ToString() };
+        }
+
+        return _response;
+    }
+
+    [HttpDelete]
+    public async Task<object> Delete(int id)
+    {
+        try
+        {
+            bool isSuccess = await _productRepository.DeleteProduct(id);
+            _response.Result = isSuccess;
+        }
+        catch (Exception e)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string>() { e.Message.ToString() };
+        }
+
+        return _response;
+    }
+}
--- Store.Services.ProductAPI/Program.cs
+++ Store.Services.ProductAPI/Program.cs
@@ -1,12 +1,20 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Store.Services.ProductAPI;
 using Store.Services.ProductAPI.DbContexts;
+using Store.Services.ProductAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
